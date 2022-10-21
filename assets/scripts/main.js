f3547624c3d0cb78f6ea87fb8c36ed8a308a658d
AndreaPanzeriDev <andpanz@libero.it>
2022-10-21 17:59:17 +0200

    con colori diversi

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,19 +10,22 @@ for(let i=1; i<=100; i++){
         if(i % 5 == 0){
             console.log("FizzBuzz");
             li.innerText += "FizzBuzz";
+            li.style.backgroundColor = "red";
         }else{
             //se non è ANCHE multiplo di 5 allora è solo multiplo di 3
             console.log("Fizz");
-            
-            li.innerText += "Fizz";     
+            li.innerText += "Fizz";
+            li.style.backgroundColor = "purple";     
         }
     }else if(i % 5 == 0){     //controllo se il numero è multiplo di 5
         console.log("Buzz");
         li.innerText += "Buzz";
+        li.style.backgroundColor = "green";
     }else{
         //in alternativa il numero non è divisibile per nessuno dei due numeri
         console.log(i);
         li.innerText += i;
+        li.style.backgroundColor = "yellow";
     }
 
     ul.append(li);
