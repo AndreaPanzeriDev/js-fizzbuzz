5b9a5a7591ceecbdf9fd56d8af6246ca56a40ebd
AndreaPanzeriDev <andpanz@libero.it>
2022-10-21 17:40:59 +0200

    lista funzionante

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,29 @@
 
 
 
-
-
 for(let i=1; i<=100; i++){
+    let ul = document.getElementById("list");
+    let li = document.createElement('li');
     //controllo se il numero è multiplo di 3
     if(i % 3 == 0){
         //controllo se il numero è ANCHE multiplo di 5
         if(i % 5 == 0){
             console.log("FizzBuzz");
+            li.innerText += "FizzBuzz";
         }else{
             //se non è ANCHE multiplo di 5 allora è solo multiplo di 3
-            console.log("Fizz");     
+            console.log("Fizz");
+            
+            li.innerText += "Fizz";     
         }
     }else if(i % 5 == 0){     //controllo se il numero è multiplo di 5
         console.log("Buzz");
+        li.innerText += "Buzz";
     }else{
         //in alternativa il numero non è divisibile per nessuno dei due numeri
         console.log(i);
-        
+        li.innerText += i;
     }
+
+    ul.append(li);
 }
