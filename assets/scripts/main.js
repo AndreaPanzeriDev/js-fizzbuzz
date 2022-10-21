00c80845f3323f1719b33e6214fc4adb701cca7e
AndreaPanzeriDev <andpanz@libero.it>
2022-10-21 15:06:13 +0200

    Buzz

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,7 +11,10 @@ for(let i=1; i<=100; i++){
             //se non è ANCHE multiplo di 5 allora è solo multiplo di 3
             console.log("Fizz");
         }
+    }else if(i % 5 == 0){     //controllo se il numero è multiplo di 5
+        console.log("Buzz");
     }else{
+        //in alternativa il numero non è divisibile per nessuno dei due numeri
         console.log(i);
     }
 }
